557f25feb258a29e09b70164bdd6102c91140ed6
lucifer-morningstar <slobeck@gmail.com>
2026-01-21 02:01:24 -0800

    Generic commit

--- scripts/gitgud.sh
+++ scripts/gitgud.sh
@@ -3,7 +3,7 @@ SHARE="$( tmux show -gqv @CHER )/lib/share"
 ICONS="$( tmux show -gqv @ICONS )"
 source "$SHARE/dump.fun"
 source "$SHARE/fatal.fun"
-#source "$SHARE/yaml2item.fun"
+source "$SHARE/yaml2item.fun"
 
 dump ">>> tmux plugin: GITGUD running..."
 
