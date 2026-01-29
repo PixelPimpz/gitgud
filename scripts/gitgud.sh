9e48390303fcef8b6701259ad075ec153332e3f0
lucifer-morningstar <slobeck@gmail.com>
2026-01-29 15:27:09 -0800

    Generic commit

--- scripts/gitgud.sh
+++ scripts/gitgud.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+LOCAL_ROOT=$1
+[[ ! $LOCAL_ROOT ]] && fatal "missing \$LOCAL_ROOT"
 SHARE="$( tmux show -gqv @CHER )"
 ICONS="$( tmux show -gqv @ICONS )"
 source "$SHARE/dump.fun"
@@ -8,11 +10,12 @@ source "$SHARE/yaml2item.fun"
 main()
 {
   dump ">>> gitgud running..."
+  [[ ! -d "$LOCAL_ROOT/.git" ]] && fatal "not a git" 
   local icon=$(yaml2item ".icons.app.gh" $ICONS)
+  local remote_url="$(git info | grep "remote\.origin\.url" )"
   dump ">> icon: $icon"
   tmux set -g '@gitgud' "$icon "
-  local remote_url="$(git info | grep "remote\.origin\.url" )"
-  dump ">> remote_url: $(git info | grep "remote\." )"
+  dump ">> remote_url: "
   tmux set -ag @gitgud "${remote_url##*github.com/} "
 
 }
