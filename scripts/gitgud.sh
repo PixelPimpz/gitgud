c499b792a0fadb858477865f83779fb8dac7481a
lucifer-morningstar <slobeck@gmail.com>
2026-01-29 06:18:30 -0800

    Generic commit

--- scripts/gitgud.sh
+++ scripts/gitgud.sh
@@ -10,7 +10,7 @@ main()
   dump ">>> gitgud running..."
   local icon=$(yaml2item ".icons.sys.GitBranch" $ICONS)
   dump ">> icon: $icon"
-  tmux set -g '@gitgud' "$icon"
+  tmux set -g '@gitgud' "$icon "
   tmux set -ag @gitgud "repo/branch"
 
 }
