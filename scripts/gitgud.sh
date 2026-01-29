36d2293aa02663d48d6343045b3edaf74a0f3439
lucifer-morningstar <slobeck@gmail.com>
2026-01-29 06:01:45 -0800

    Generic commit

--- scripts/gitgud.sh
+++ scripts/gitgud.sh
@@ -7,22 +7,7 @@ source "$SHARE/yaml2item.fun"
 
 main()
 {
-  dump ">>> tmux plugin: GITGUD running..."
-  local info="$(grep -e 'remote.origin.url' <$( git-info ))"
-  echo "$info"
-  local git_name="$(git-info | grep "^origin.*push" | sed  's/^.*://;s/\..*$//')"
-  dump ">> line: $info"
-  dump ">> git_name: $git_name"
-  icon=$(yaml2item ".icons.sys.GitBranch" "$ICONS")
-  (( "$?" != 0 )) && fatal "failed to get icon" || dump ">> GitBranch: $icon" 
-  tmux set -g @GitBranch "$icon"
-  tmux set -ag @GitBranch "$git_name"
-
-  
-  unit $icon
+  dump ">>> gitgud running..."
 }
 
-unit(){
-  tmux set -g @Gitgud "#[fg=#{@green}]#{@HemiL}#[bg=#{@bg0}]#[reverse]#{@GitBranch} #[bg=default]#[noreverse]#{@TriangleR}"
-}
 main
